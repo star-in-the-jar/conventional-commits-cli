4b82e561d4690073f1d3044cdc4cf9e9600bc15a
Stanisław Jarocki <s.jarocki354@gmail.com>
2024-05-20 21:30:43 +0200

    Create basic structure & types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,57 @@
-const tsAdded = (): string => "Success: Typescript has been added."
+import os from 'os';
+import path from 'path';
+import config from '../config.json'
 
-console.log(tsAdded())
+const choices = ['Option 1', 'Option 2', 'Option 3'];
+
+const questions = [
+    {
+        type: 'list',
+        name: 'choice',
+        message: 'Please select an option:',
+        choices: choices,
+    },
+];
+
+const gitmojis = config.gimojis.path
+    ? require(config.gimojis.path)
+    : require(path.join(os.homedir(), '.gitmoji', 'gitmojis.json'));
+
+interface DataObj {
+    name: string
+    value: string
+    description?: string
+}
+
+type Type = string
+type TypeObj = DataObj
+type Gitmoji = string
+type GitmojiObj = DataObj
+type Scope = string
+
+interface ScopeObj extends DataObj {
+    complementary?: string
+}
+
+interface Config {
+    type: TypeObj[];
+    scope: ScopeObj[];
+    gimojis: {
+        path: string;
+    }
+    favorites: {
+        gitmojis: Gitmoji[];
+        scopes: Scope[];
+        types: Type[];
+    }
+}
+
+const main = (): void => {
+
+}
+
+const getTypes = (): TypeObj[] => config.type
+
+const getScopes = (): ScopeObj[] => config.scope
+
+const getGitmojis = (): GitmojiObj[] => gitmojis
